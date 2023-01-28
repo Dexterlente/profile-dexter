4625f5a69ad5c2fd23f2c3783c15e2b3b3f24ace
Dexterlente <dexter.lente18@gmail.com>
2023-01-29 07:59:43 +0800

    te

--- src/components/TechnologyPage.js
+++ src/components/TechnologyPage.js
@@ -1,17 +1,143 @@
 import React from 'react'
 import FullScreenSection from './FullScreenSection'
+import { VStack, HStack, Center, Square,  } from '@chakra-ui/react'
 
 
 const TechnologyPage = () => {
-  return ( <FullScreenSection
-    color="black"
-    backgroundColor="white"
-    cursor="pointer"
-    borderRadius="xl">
-
-        <div>TechnologyPage</div>
-
+  return ( <FullScreenSection backgroundColor="#00038B">
+            <VStack spacing='90px'className="">
+            <HStack spacing='100px' mt="100px">
+                <Center w='130px' h='130px'>
+                    <Square size='130px'>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" />
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                       
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" />          
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                 
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" />            
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />               
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                                             
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />                      
+                        </Square>
+                    </Center>
+            </HStack>
+            <HStack spacing='100px'>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                                       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />                       
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />                     
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain-wordmark.svg" />                     
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />                     
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg" />                     
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" />                 
+                        </Square>
+                    </Center>
+            </HStack>
+            <HStack spacing='100px'> 
+                     <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg" />               
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />                
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" />                
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />               
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/babel/babel-original.svg" />              
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg" />              
+                        </Square>
+                    </Center>
+            </HStack>
+            <HStack spacing='100px'>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" />            
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />             
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-plain-wordmark.svg" />            
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain-wordmark.svg" />           
+                        </Square>
+                    </Center>
+                    <Center w='130px' h='130px'>
+                        <Square size='130px'>                                                  
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg" />            
+                        </Square>
+                    </Center>
+                </HStack>
+            </VStack>
     </FullScreenSection>
+
+    
   )
 }
 
